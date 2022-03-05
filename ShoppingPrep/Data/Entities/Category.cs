7398912a6f2a39a172103cbf5204c012edcd22a0
rafaelsantos1000 <rafaasfs@gmail.com>
2022-03-05 18:54:05 +0000

    Categories CRUD finished

---
+++ ShoppingPrep/Data/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingPrep.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+
+        [Display(Name = "Categoria")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Name { get; set; }
+    }
+}
