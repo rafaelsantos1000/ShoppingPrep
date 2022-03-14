d1e935707457669df5132b44fe589c6a771aa9e5
rafaelsantos1000 <rafaasfs@gmail.com>
2022-03-14 16:04:29 +0000

    part 8 - finish States and Cities CRUD

--- ShoppingPrep/Data/Entities/City.cs
+++ ShoppingPrep/Data/Entities/City.cs
@@ -7,7 +7,7 @@ namespace ShoppingPrep.Data.Entities
         public int Id { get; set; }
 
 
-        [Display(Name = "Ciudad")]
+        [Display(Name = "Cidade")]
         [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} carateres.")]
         [Required(ErrorMessage = "O campo {0} é obrigatório.")]
         public string Name { get; set; }
---
+++ ShoppingPrep/Models/CityViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingPrep.Models
+{
+    public class CityViewModel
+    {
+        public int Id { get; set; }
+
+
+        [Display(Name = "Cidade")]
+        [MaxLength(50, ErrorMessage = "O campo {0} deve ter no máximo {1} caracteres.")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public string Name { get; set; }
+
+
+        public int StateId { get; set; }
+    }
+}
